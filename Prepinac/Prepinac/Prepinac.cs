76a69dd1321d6d6b96735637a6e0e7d300818f26
Michail <50227949+Michail77@users.noreply.github.com>
2020-05-25 16:14:38 +0200

    Add files via upload

---
+++ Prepinac/Prepinac/Prepinac.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Prepinac
+{
+    class Prepinac
+    {
+        public delegate void ThreadStart();
+        public void Vypisuj0()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("0");
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Vypisuj1()
+        {
+            for (int i = 0; i <5; i++)
+            {
+                Console.Write("1");
+                Thread.Sleep(10);
+            }
+        }
+
+        //public void Prepinej()
+        //{
+        //    Thread vlakno = new Thread(Vypisuj0);
+        //    vlakno.Start();
+        //    Vypisuj1();
+        //}
+    }
+}
---
+++ Prepinac/Prepinac/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Prepinac
+{
+    class Program
+    {
+        public delegate void ThreadStart();
+        static void Main(string[] args)
+        {
+            Prepinac p = new Prepinac();
+            Thread vlakno1 = new Thread(p.Vypisuj0);
+            Thread vlakno2 = new Thread(p.Vypisuj1);
+            vlakno1.Start();
+            vlakno2.Start();
+            vlakno1.Join();
+            vlakno2.Join();
+            Console.WriteLine("Hotovo");
+        }
+    }
+}
